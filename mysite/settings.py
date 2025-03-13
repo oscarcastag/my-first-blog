5deb03609f30755a58fa705b973dd480f31ee9ed
oscarcg <oscastag@hotmail.com>
2025-03-13 17:27:12 -0600

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
